61816604cfd63361707d33e836f6218e71727ec8
petar-stojanovic <petar-stojanovic@github.com>
2024-03-02 18:53:29 +0100

    added firebase

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,23 @@
-import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import {ApplicationConfig, importProvidersFrom} from '@angular/core';
+import {provideRouter, withViewTransitions} from '@angular/router';
 
-import { routes } from './app.routes';
+import {routes} from './app.routes';
+import {initializeApp, provideFirebaseApp} from '@angular/fire/app';
+import {getAuth, provideAuth} from '@angular/fire/auth';
+import {getFirestore, provideFirestore} from '@angular/fire/firestore';
+import {getDatabase, provideDatabase} from '@angular/fire/database';
+import {getStorage, provideStorage} from '@angular/fire/storage';
+import {environment} from "../environments/environment";
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [provideRouter(routes, withViewTransitions()),
+    importProvidersFrom([
+
+      provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
+      provideAuth(() => getAuth()),
+      provideFirestore(() => getFirestore()),
+      provideDatabase(() => getDatabase()),
+      provideStorage(() => getStorage())
+    ]),
+  ]
 };
---
+++ src/environments/environment.development.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  firebaseConfig: {
+    apiKey: "AIzaSyCsUMJEGUU7iT9ce9RxPdqVMEg9ZRcnm_Q",
+    authDomain: "platemate-a1ca9.firebaseapp.com",
+    projectId: "platemate-a1ca9",
+    storageBucket: "platemate-a1ca9.appspot.com",
+    messagingSenderId: "618038627283",
+    appId: "1:618038627283:web:58ee2a9da71d136bf23d26"
+  }
+};
